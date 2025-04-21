19ca5e3e3d29494ee3f818ac659eac88974428b0
migueruokumura <migueruokumura@gmail.com>
2025-04-21 11:13:01 -0500

    Tipos de datos numéricos y de texto

---
+++ Introduccion/tiposDeDatos.js
@@ -0,0 +1,15 @@
+//Cadena de Texto
+let nombre = "Miguel"; //comillas sencillas '', comillas dobles "".
+let parrafo = "este es un 'parrafo'";
+//let parrafo = "este es un \'parrafo\''"
+
+//Número
+const num = 4;
+const num2 = -8;
+
+//booleano
+const userConnected = true;
+const userDisconnected = false;
+
+const mayorQue = 1 > 5;
+console.log(mayorQue);
