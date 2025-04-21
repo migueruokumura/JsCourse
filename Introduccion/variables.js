39dee22c30c99c7fda26ba70912cdf78d7f1916d
migueruokumura <migueruokumura@gmail.com>
2025-04-21 10:51:15 -0500

    Variables y tipos de datos

--- Introduccion/variables.js
+++ Introduccion/variables.js
@@ -1,1 +1,20 @@
-let hola = "Hola Mundo";
+let nombre = "Miguel Leonardo"; //funciona en el bloque de código en que se declara, se le puede cambiar el valor
+nombre = "Migueru Okumara";
+const correo = "migueruokumura@gmail.com"; //también solo se puede acceder deentro de un bloque de código, no se le puede cambiar el valor
+var edad = 26; //puede funcionar globalmente, esta es la forma antigua de grabar variables puede generar problemas pero se puede seguir usando.
+console.log(edad);
+
+let telefono, pais, id; //definir varias variables en una sola linea.|
+telefono = 3223026598;
+
+const resultado = 4 + 4; // operaciones aritméticas
+console.log(resultado);
+
+const nombre1 = "Carlos ";
+const nombre2 = "Juanito";
+
+const nombreCompleto = nombre1 + nombre2; //se concatenan textos
+console.log(nombreCompleto);
+
+let miVariable = "texto"; //se puede cambiar el tipo de dato
+miVariable = 7;
