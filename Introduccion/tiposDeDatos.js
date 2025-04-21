8fa8544ff27bd3389ea230e4a6e3b6700fd8cfd7
migueruokumura <migueruokumura@gmail.com>
2025-04-21 11:38:22 -0500

    Tipos de datos arreglo, objetos, funciones, null y undefined

--- Introduccion/tiposDeDatos.js
+++ Introduccion/tiposDeDatos.js
@@ -13,3 +13,31 @@ const userDisconnected = false;
 
 const mayorQue = 1 > 5;
 console.log(mayorQue);
+
+//arrays
+const arreglo = [1, "jean", true, { propiedad: "valor" }, [1, 2, 3]]; //guarda valores
+console.log(arreglo);
+
+//objetos
+const persona = {
+  edad: 26,
+  nombre: "Miguel",
+  electrocutado: true,
+  carro: {
+    marca: "bmw",
+    color: "azul",
+  },
+}; //guarda propiedades y sus valores
+console.log(persona.electrocutado);
+
+//function
+function escribirHola() {
+  console.log("hola zapatin con cola");
+}
+escribirHola();
+
+//null
+let variableNull = null;
+
+//undefined
+let variableSinDefinir;
